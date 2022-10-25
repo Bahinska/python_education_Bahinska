79e6eb9a6fdd5e930477744d9b76a79be96fa132
Margo <97248807+Bahinska@users.noreply.github.com>
2022-10-26 00:12:53 +0200

    Update Observer.py

--- Навчальна практика/Np_task6_observer/Observer/Observer.py
+++ Навчальна практика/Np_task6_observer/Observer/Observer.py
@@ -1,16 +1,10 @@
 from Logger import Logger
+
+
 class Observer:
-    _observers = []
 
     def __init__(self):
-        self._observers.append(self)
-        self._observe_events = {}
+        self._list = {}
 
-    def observe_event(self, event_name, callback):
-        self._observe_events[event_name] = callback
-
-    def not_observe_event(self, event_name):
-        if event_name in self._observe_events.keys():
-            del self._observe_events[event_name]
-        else:
-            raise Exception("This event isn't observed")
+    def new(self, key, func):
+        self._list[key] = func
