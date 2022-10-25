0c676701d5b5d95703751b2e11772e0a5bf70d60
Margo <97248807+Bahinska@users.noreply.github.com>
2022-10-26 00:14:03 +0200

    Update Event.py

--- Навчальна практика/Np_task6_observer/Observer/Event.py
+++ Навчальна практика/Np_task6_observer/Observer/Event.py
@@ -1,19 +1,15 @@
 from Observer import Observer
 
 class Event:
-    def __init__(self, name, changed, posOrRange, before, log=True):
+    def __init__(self, name, changed, posOrRange, before):
         self.name = name
         self.changed = changed
         self.posOrRange = posOrRange
         self.before = before
 
-        if log:
-            self.log_event()
-
-    def log_event(self):
-        for observer in Observer._observers:
-            if self.name in observer._observe_events:
-                observer._observe_events[self.name](self)
+    def new_event(self, ob):
+        if self.name in ob._list:
+            ob._list[self.name](self)
 
     def __str__(self):
         return f"{self.name}. New list: [{self.changed}], by/on position {self.posOrRange}, Previous list: [{self.before}]"
